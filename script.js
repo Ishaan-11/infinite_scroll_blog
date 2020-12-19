8e0b3410b44d5c5eae5e2e282c1b21da94104bd8
Ishaan-11 <ishaansharma1998@gmail.com>
2020-12-19 12:45:51 +0530

    add: script

---
+++ script.js
@@ -0,0 +1,79 @@
+const filter = document.getElementById('filter'),
+  postsContainer = document.getElementById('posts-container'),
+  loader = document.querySelector('.loader');
+
+
+let page = 1;
+let limit = 5;
+
+// Fetch posts from API
+async function getPostsApi() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`);
+  const data = await response.json();
+
+  return data;
+}
+
+// Show posts in DOM
+async function showPosts() {
+  const posts = await getPostsApi();
+
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+// Show loader & fetch more posts
+function loadMorePost() {
+  loader.classList.add('show');
+
+  setTimeout(() => {
+    loader.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+// Filter posts by input
+function filterPosts (e) {
+  let term = e.target.value.toUpperCase();
+
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
+// Show initial posts
+showPosts();
+
+window.addEventListener('scroll', () => {
+  const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    loadMorePost();
+  }
+})
+
+filter.addEventListener('input', filterPosts);
